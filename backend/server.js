cc8f8659abff6d4bdce825571b3e9889c9b565c1
Alyssa Voccia <avoccia@hotmail.com>
2023-09-04 10:49:50 -0400

    Working on adding mongo and mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import exrpress from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
 const port = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
 
 const app = exrpress();
 
